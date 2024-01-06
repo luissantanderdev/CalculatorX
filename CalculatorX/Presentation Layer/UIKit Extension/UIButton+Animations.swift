fcc22380029086b10a3dac42d208166c26cf9702
Luis Santander <luissantanderdev@gmail.com>
2024-01-06 08:16:08 -0700

    setup logic for UIButton animations in order to have spring damping on the buttons to give it an organic feel to the calculator.

--- CalculatorX/Presentation Layer/UIKit Extension/UIButton+Animations.swift
+++ CalculatorX/Presentation Layer/UIKit Extension/UIButton+Animations.swift
@@ -8,11 +8,75 @@
 import Foundation
 import UIKit
 
+/**
+ NOTES:
+ -------------------------------------
+ UIView is the root class of all views in UIKit for iOS
+ 
+ if you want to be pro at animations you need to either ease in animations or ease out animations. Experiment.
+ */
+
+
 extension UIButton {
     
     func bounce() {
-        print("Break")
-        print("It Works")
+        moveUp()
+       // moveUpWithSpringDamping() // use this to use damping function which is the same as moveUp functionality
+    }
+    
+    func growLarger() {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn) {
+            self.transform = CGAffineTransform(scaleX: 4, y: 4)
+        } completion: { _ in
+            // do nothing but this fires after animation fires.
+        }
+    }
+    
+    func makeSmaller() {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn) {
+            self.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        } completion: { _ in
+            // do nothing but this fires after animation fires.
+        }
+    }
+    
+    func moveUp() {
+        // let imageView = UIImageView() // if you were to use an image
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) {
+            self.transform = CGAffineTransform(translationX: 0, y: -50)
+        } completion: { _ in
+            self.moveDown()
+        }
+    }
+    
+    func moveDown() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+            self.transform = CGAffineTransform(translationX: 0, y: 5)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+                self.transform = CGAffineTransform(translationX: 0, y: -2)
+            } completion: { _ in
+                UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+                    self.transform = CGAffineTransform(translationX: 0, y: 0)
+                } completion: { _ in
+                    // do nothing
+                }
+            }
+        }
+    }
+    
+    func moveUpWithSpringDamping() {
+        UIView.animate(withDuration: 0.1, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseOut) {
+            self.transform = CGAffineTransform(translationX: 0, y: 50)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: .curveEaseInOut) {
+                self.transform = CGAffineTransform(translationX: 0, y: 0)
+            } completion: { _ in
+                // do nothing
+            }
+        }
+        
     }
 }
 
